f496eb9d94f75c9242a01ec4f88ce9607078a8ec
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-07-20 00:28:31 +0100

    Updated bower.json and the program now runs successfully

--- src/Main.purs
+++ src/Main.purs
@@ -4,7 +4,7 @@ import Prelude
 import Data.List
 import Data.Maybe
 import Data.Tuple
--- import Test.QuickCheck
+import Test.QuickCheck
 
 -- Main.runLengthEnc $ 1:1:1:2:2:3:Nil
 runLengthEnc :: forall a. (Eq a) => List a -> List (Tuple Int a)
@@ -19,11 +19,10 @@ runLengthEnc as =
 runLengthDec :: forall a. List (Tuple Int a) -> List a
 runLengthDec = concatMap (uncurry replicate)
 
--- prop :: Number -> Number -> Result
--- prop x y =
---   let result = x * x + y * y >= x * x
---   in result <?> "x: " ++ show x ++ "; y: " ++ show y
+prop :: Number -> Number -> Result
+prop x y =
+  let result = x * x + y * y >= x * x
+  in result <?> "x: " ++ show x ++ "; y: " ++ show y
 
 main = do
-  -- quickCheck prop
-  unit
+  quickCheck prop
